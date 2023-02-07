89fa7b24332923daaedba96c2af0e700c99c9c27
kien <you@example.com>
2023-02-07 13:03:04 +0700

    update ResetPass service

--- service/resetPass.service.js
+++ service/resetPass.service.js
@@ -1,16 +1,16 @@
 const userModel = require('../models/user.model');
 
-const reserPass = async (req, res, next) => {
+const resetPass = async (req, res, next) => {
 
     try {
-        const check = await userModel.find({ email: req.body.email })
+        const checkUser = await userModel.find({ email: req.body.email })
 
-        if (check.length > 0) {
+        if (checkUser.length > 0) {
 
             await userModel.updateOne({ email: req.body.email }, { password: '2002' })
             return res.status(200).json("2002")
         }
-        else if (check.length == 0) {
+        else {
             return res.status(400).json("Email error")
         }
     }
@@ -19,4 +19,5 @@ const reserPass = async (req, res, next) => {
     }
 
 }
-module.exports = { reserPass }
+module.exports = { resetPass }
+
